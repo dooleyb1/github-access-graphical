49bb0c961ea612611d6d1ef00e6af4753fa2eb52
dooleyb1 <dooleyb1@tcd.ie>
2018-10-31 21:14:49 +0000

    nice tidy username form, no functionality

--- app/src/Form.js
+++ app/src/Form.js
@@ -1,52 +1,25 @@
 import React, { Component } from 'react';
-import { FormErrors } from './FormErrors';
 import './Form.css';
 
 class Form extends Component {
+
   constructor (props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
-      formErrors: {username: '', password: ''},
-      usernameValid: false,
-      passwordValid: false,
-      formValid: false
-    }
+      username: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleUserInput = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({[name]: value},
-                  () => { this.validateField(name, value) });
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
-  validateField(fieldName, value) {
-    let fieldValidationErrors = this.state.formErrors;
-    let usernameValid = this.state.usernameValid;
-    let passwordValid = this.state.passwordValid;
-
-    switch(fieldName) {
-      case 'username':
-        usernameValid = value.length >= 6;
-        fieldValidationErrors.username = usernameValid ? '' : ' is invalid';
-        break;
-      case 'password':
-        passwordValid = value.length >= 6;
-        fieldValidationErrors.password = passwordValid ? '': ' is too short';
-        break;
-      default:
-        break;
-    }
-    this.setState({formErrors: fieldValidationErrors,
-                    usernameValid: usernameValid,
-                    passwordValid: passwordValid
-                  }, this.validateForm);
-  }
-
-  validateForm() {
-    this.setState({formValid: this.state.usernameValid && this.state.passwordValid});
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
   }
 
   errorClass(error) {
@@ -55,26 +28,16 @@ class Form extends Component {
 
   render () {
     return (
-      <form className="demoForm">
-        <h2>Login</h2>
+      <form className="demoForm" onSubmit={this.handleSubmit}>
+        <label htmlFor="username">GitHub Access</label>
         <div className="panel panel-default">
-          <FormErrors formErrors={this.state.formErrors} />
         </div>
-        <div className={`form-group ${this.errorClass(this.state.formErrors.username)}`}>
-          <label htmlFor="username">GitHub Username</label>
-          <input type="username" required className="form-control" name="username"
+        <div className={`form-group`}>
+          <input type="text" required className="form-control" name="username"
             placeholder="GitHub Username"
-            value={this.state.username}
-            onChange={this.handleUserInput}  />
+            onChange={this.handleChange}/>
         </div>
-        <div className={`form-group ${this.errorClass(this.state.formErrors.password)}`}>
-          <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" name="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleUserInput}  />
-        </div>
-        <button type="submit" className="btn btn-primary" disabled={!this.state.formValid}>Sign up</button>
+        <button type="submit" className="btn btn-primary">Find Me</button>
       </form>
     )
   }
