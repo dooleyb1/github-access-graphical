5b72dc34ce07548dee121bc5d627cb117ea06724
dooleyb1 <dooleyb1@tcd.ie>
2018-11-05 14:20:20 +0000

    added gitignore & more styling

--- src/UserCard.js
+++ src/UserCard.js
@@ -6,44 +6,30 @@ class UserCard extends Component {
 
   render () {
     return (
-      // <div className="User-card">
-      //   <img className="User-icon" src={this.props.data.avatar_url} alt="user icon"/>
-      //   <div className="User-details">
-      //     <p>{this.props.data.name}</p>
-      //     <p>{this.props.data.bio}</p>
-      //     <p>Followers: {this.props.data.followers} | Following: {this.props.data.following}</p>
-      //     <p>Public Repos: {this.props.data.public_repos}</p>
-      //     <a className="User-blog" href={this.props.data.blog} target="_blank" rel="noopener noreferrer">Blog</a>
-      //   </div>
-      //   <div className="Button">
-      //     <button type="submit" className="btn btn-primary" onClick={this.props.onReturn}>Return</button>
-      //   </div>
-      // </div>
-      <div className="user-card">
-        <Row vertical='center' horizontal='space-between'>
-          <Column flexGrow={1} horizontal='center'>
-            <img className="user-icon" src={this.props.data.avatar_url} alt="user icon"/>
-            <div className="user-details">
-              <p>{this.props.data.name}</p>
-              <p>{this.props.data.bio}</p>
-              <p>Followers: {this.props.data.followers} | Following: {this.props.data.following}</p>
-              <p>Public Repos: {this.props.data.public_repos}</p>
+        <div className="container">
+          <div className="g grid-45">
+            <div className="user-card">
+              <img className="user-icon" src={this.props.data.avatar_url} alt="user icon"/>
+              <div className="user-details">
+                <p>{this.props.data.name}</p>
+                <p>{this.props.data.bio}</p>
+                <p>Followers: {this.props.data.followers} | Following: {this.props.data.following}</p>
+                <p>Public Repos: {this.props.data.public_repos}</p>
+              </div>
+              <a className="user-blog" href={this.props.data.blog} target="_blank" rel="noopener noreferrer">Blog</a>
+              <div className="return-button">
+                <button type="submit" className="btn btn-primary" onClick={this.props.onReturn}>Return</button>
+              </div>
             </div>
-            <a className="user-blog" href={this.props.data.blog} target="_blank" rel="noopener noreferrer">Blog</a>
-            <div className="return-button">
-              <button type="submit" className="btn btn-primary" onClick={this.props.onReturn}>Return</button>
-            </div>
-          </Column>
-          <Column flexGrow={1} horizontal='center'>
-            <h3> Column 2 </h3>
-            <span> column 2 content </span>
-          </Column>
-          <Column flexGrow={1} horizontal='center'>
+          </div>
+          <div className="g grid-5">
+            <div className="vertical-line"></div>
+          </div>
+          <div className="g grid-45">
             <h3> Column 3 </h3>
             <span> column 3 content </span>
-          </Column>
-        </Row>
-      </div>
+          </div>
+        </div>
     )
   }
 }
