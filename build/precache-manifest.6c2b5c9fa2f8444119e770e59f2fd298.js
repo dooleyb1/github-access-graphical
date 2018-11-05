3fc7568216f256385b014e3a6bc4c221740df981
dooleyb1 <dooleyb1@tcd.ie>
2018-11-05 10:26:55 +0000

    changes for server-side hosting

---
+++ build/precache-manifest.6c2b5c9fa2f8444119e770e59f2fd298.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "82b237518b2ab59481b3",
+    "url": "/static/css/main.8029bfe7.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "262929b0c8d99877a7ab",
+    "url": "/static/js/1.262929b0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a91f24e35688f0f97e9e39406e059b21",
+    "url": "/static/media/gh2.a91f24e3.png"
+  },
+  {
+    "revision": "82b237518b2ab59481b3",
+    "url": "/static/js/main.82b23751.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "262929b0c8d99877a7ab",
+    "url": "/static/css/1.060c736e.chunk.css"
+  },
+  {
+    "revision": "24883f04e29829a0db17fe1ae23881ef",
+    "url": "/index.html"
+  }
+];
