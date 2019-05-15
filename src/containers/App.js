b61bf907b8f7a9b4f56231858f17d33f95118eb8
dooleyb1 <dooleyb1@tcd.ie>
2019-05-15 16:46:09 +0100

    pushing final changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import Form from './Form.js';
-import UserPage from './UserPage.js';
-const octokit = require('@octokit/rest')();
 
 class App extends Component {
 
@@ -30,28 +28,6 @@ class App extends Component {
 
     event.preventDefault();
 
-    // Get User details from REST API
-    octokit.users.getForUser({username: this.state.username}, (error, result) => {
-      if(error){
-        this.setState({isValidUsername: false});
-        return
-      } else {
-        this.setState({isValidUsername: true});
-        this.setState({userData: result.data});
-      }
-    });
-
-    // Get User Repo details from REST API
-    octokit.repos.getForUser({username: this.state.username}, (error, result) => {
-      if(error){
-        this.setState({isValidUsername: false})
-        return
-      } else {
-        this.setState({isValidUsername: true});
-        this.setState({repoData: result.data});
-        this.setState({submitted: true});
-      }
-    });
   }
 
   handleReturn(event) {
@@ -68,7 +44,6 @@ class App extends Component {
         <img src={ require('../images/gh2.png') } className="app-logo" alt="logo" />
         <div className="app-container">
           {!this.state.submitted && <Form onChangeValue={this.handleChange} onSubmit={this.handleSubmit} isValidUsername={this.state.isValidUsername}/>}
-          {this.state.submitted && this.state.isValidUsername && <UserPage userData={this.state.userData} repoData={this.state.repoData} onReturn={this.handleReturn}/>}
         </div>
       </div>
     );
